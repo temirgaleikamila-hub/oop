0dcdb5cc951b1d4ac9ff6b3644d2824c866a4efe
Kamila <temirgaleikamila@gmail.com>
2025-12-16 00:47:22 +0500

    main class and donor

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,11 @@
-public class Main{
-    public static void main(String[] args){
-    Donor donor1 = new Donor("Kamila");
-    Donor donor2 = new Donor("Danial");
-    Donor donor3 = new Donor("Yasmina");
+public class Main {
+    public static void main(String[] args) {
 
-    Donation donation1 = new Donation(donor1, 5000, "Pet Support");
-    Donation donation2 = new Donation(donor2, 15000, "Food Support");
-    Donation donation3 = new Donation(donor3, 10000, "Medical Support");
+        Donor d1 = new Donor(101010, "Kamila", "k@mail.com", "+7701...", "Individual");
+        Donor d2 = new Donor(202020, "Aigul", "a@mail.com", "+7702...", "Corporate");
 
-    Charity charity1 = new Charity("Helping pets", "Helping families in need", "Helping ill patients");
-    System.out.println("Donor:");
-    System.out.println(donor1);
-    System.out.println(donor2);
-    System.out.println(donor3);
-
-    System.out.println("Donation:");
-    System.out.println(donation1);
-    System.out.println(donation2);
-    System.out.println(donation3);
-
-    System.out.println("Charity organization:");
-    System.out.println(charity1);
-
-
-        //create donor, donation and charity objects
-
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println("d1 equals d2? " + d1.equals(d2));
     }
 }
