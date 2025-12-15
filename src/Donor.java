ba48414cc61093ff648f8427fdab1028ded0bdd8
Kamila <temirgaleikamila@gmail.com>
2025-12-16 00:40:52 +0500

    donor class

---
+++ src/Donor.java
@@ -0,0 +1,69 @@
+public class Donor {
+
+    private int donorId;
+    private String fullName;
+    private String email;
+    private String phone;
+    private String donorType;
+
+    public Donor(int donorId, String fullName, String email, String phone, String donorType) {
+        this.donorId = donorId;
+        this.fullName = fullName;
+        this.email = email;
+        this.phone = phone;
+        this.donorType = donorType;
+    }
+
+    //using getter and setter
+    public int getDonorId() {
+        return donorId;
+    }
+
+    public void setDonorId(int donorId) {
+        this.donorId = donorId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDonorType() {
+        return donorType;
+    }
+
+    public void setDonorType(String donorType) {
+        this.donorType = donorType;
+    }
+
+    public boolean equals(Donor other) {
+        return other != null && this.donorId == other.donorId;
+    }
+
+    public String toString() {
+        return "Donor{id=" + donorId +
+                ", name='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", type='" + donorType + "'}";
+    }
+}
--- src/hello.java
+++
@@ -1,5 +0,0 @@
-public class hello {
-    public void main(String[] args){
-        System.out.println("hello");
-    }
-}
