b96e24eab9f6316f64b33c1043301af504369880
Kamila <temirgaleikamila@gmail.com>
2025-12-16 14:22:33 +0500

    char

--- src/Charity.java
+++ src/Charity.java
@@ -1,2 +1,62 @@
 public class Charity {
+
+    private int charityId;
+    private String name;
+    private String category;
+    private String email;
+    private boolean active;
+
+    public Charity(int charityId, String name, String category, String email) {
+        this.charityId = charityId;
+        this.name = name;
+        this.category = category;
+        this.email = email;
+    }
+
+    public int getCharityId() {
+        return charityId;
+    }
+
+    public void setCharityId(int charityId) {
+        this.charityId = charityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public String toString() {
+        return "Charity{id=" + charityId +
+                ", name='" + name +
+                ", category='" + category +
+                ", email='" + email + "}";
+    }
+
+    public boolean equals(Charity other) {
+        return other != null && this.charityId == other.charityId;
+    }
 }
