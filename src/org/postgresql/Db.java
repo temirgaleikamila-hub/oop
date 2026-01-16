ab94c02dab333f0c597334788f8d9c892a945987
Kamila <temirgaleikamila@gmail.com>
2026-01-16 21:58:27 +0500

    oop4

--- src/org/postgresql/Db.java
+++ src/org/postgresql/Db.java
@@ -3,9 +3,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class Db {
-    private static final String URL = "jdbc:postgresql://localhost:2710/CharityDonationSystem";
+    private static final String URL = "jdbc:postgresql://localhost:2710/CharitySystem";
     private static final String USER = "postgres";
-    private static final String PASS = "Alimaks28!";
+    private static final String PASS = "123";
     public static Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USER, PASS);
