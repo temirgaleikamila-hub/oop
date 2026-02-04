0fc5fb4b92da42d13599e2ad5d5374868e1a837a
Kamila <temirgaleikamila@gmail.com>
2026-02-05 02:10:28 +0500

    oop

--- src/main/java/com/example/demo/DonorController.java
+++ src/main/java/com/example/demo/DonorController.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- src/main/java/com/example/demo/DonorRepository.java
+++ src/main/java/com/example/demo/DonorRepository.java
@@ -1,7 +1,5 @@
 package com.example.demo;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface DonorRepository extends JpaRepository<Donor, Integer> {
